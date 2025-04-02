e4f391675e29fe913aa5885e5e4393fa4e1e57f7
molinwu <858459615@qq.com>
2025-04-02 04:09:58 -0700

    clippy and conversion

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let spl:Vec<_> = s.split(",").collect();
+        if spl.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if spl[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        match spl[1].parse() {
+            Ok(a) =>Ok(Person{name:spl[0].to_string(),age:a}),
+            Err(b)=> Err(ParsePersonError::ParseInt(b))
+        }
+      
     }
 }
 
